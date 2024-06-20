ccafb52193fda6f5f5f8eb18cc43ecdce2eb21db
Carolina Denisse Paleari <palearicaro@gmail.com>
2024-06-20 14:26:34 -0300

    Tabs.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatTabsModule } from '@angular/material/tabs';
 import { TableComponent } from './components/table/table.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TableComponent],
+  imports: [RouterOutlet, TableComponent, MatTabsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
