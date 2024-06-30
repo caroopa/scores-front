b106a1e6c115405396877cc4bcdc105b1f497bec
Carolina Denisse Paleari <63772641+caroopa@users.noreply.github.com>
2024-06-30 09:34:56 -0300

    Update config.ts

--- src/app/services/config.ts
+++ src/app/services/config.ts
@@ -1,2 +1,2 @@
-export const apiURL = 'https://scores-back.onrender.com/';
-export const apiURLws = 'scores-back.onrender.com';
+export const apiURL = 'http://127.0.0.1:8000/';
+export const apiURLws = '127.0.0.1:8000';
